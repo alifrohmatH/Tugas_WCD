04ea8fb296c40e38b9c49729a891639d6ef3f736
alifrohmatH <alif23ez@gmail.com>
2025-03-15 21:30:26 +0700

    type

--- Tugas2/1-convertCelcius.js
+++
@@ -1,6 +0,0 @@
-const fahrenheit = 10
-
-
-const rumus = (fahrenheit - 32) / 1.8
-
-console.log(rumus)
---
+++ Tugas2/1-convertCelcius.ts
@@ -0,0 +1,6 @@
+const fahrenheit: number = 10
+
+
+const rumus: number = (fahrenheit - 32) / 1.8
+
+console.log(rumus)
