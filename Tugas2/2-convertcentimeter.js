5a845ba626aceaa0d5387c62c960575f97609cc7
alifrohmatH <alif23ez@gmail.com>
2025-03-16 01:13:47 +0700

    js

--- Tugas2/2-convertcentimeter.js
+++
@@ -1,5 +0,0 @@
-const centimeter = 10000;
-
-const convert = centimeter / 1000;
-
-console.log(convert); // hasil: 10
---
+++ Tugas2/Soal 2-.js
@@ -0,0 +1,5 @@
+const centimeter = 300000;
+
+const convert = centimeter / 100000;
+
+console.log(convert); // hasil: 3 km
